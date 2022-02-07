962aea79e7453bff377336e3ac48805bf697ff2a
yongchulbag <yongchulbag@gmail.com>
2022-02-07 23:46:45 +0900

    집에서

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0F0F1E),
+        accentColor: Colors.brown,
+      ),
       home: InputPage(),
     );
   }
