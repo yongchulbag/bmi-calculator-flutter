328df1ad8bfed856e2629cf606edbfa847b90ab4
flype <ggg>
2022-02-10 19:52:11 +0900

    도서관에

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,10 +9,81 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body:
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+                child: Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Primary_Card(selected_color : Color(0XFF1D1E33)),
+                ),
+                Expanded(
+                  child: Primary_Card(selected_color : Color(0XFF1D1E33)),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+              ],
+            )),
+            Expanded(
+                child: Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Primary_Card(selected_color : Color(0XFF1D1E33)),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+              ],
+            )),
+            Expanded(
+                child: Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Primary_Card(selected_color : Color(0XFF1D1E33)),
+                ),
+                Expanded(
+                  child: Primary_Card(selected_color : Color(0XFF1D1E33)),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+              ],
+            )),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ));
+  }
+}
+
+class Primary_Card extends StatelessWidget {
+  Primary_Card({@required this.selected_color});
+  final Color selected_color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: selected_color,
+        borderRadius: BorderRadius.circular(20),
       ),
     );
   }
