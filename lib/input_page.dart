9e8ce2a2c23e3c2c657c3b7b3c91ac61cee4476f
황등종로약국 <flypeace@naver.com>
2022-02-17 18:32:01 +0900

    약국에서

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,12 +3,21 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'primary_card.dart';
 import 'inside_card.dart';
 
+const Color activatedCardColor = Color(0XFF1D1E33);
+const Color inactivatedCardColor = Color(0XFF111328);
+enum Gender {
+  male,
+  female,
+}
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,20 +36,38 @@ class _InputPageState extends State<InputPage> {
                   width: 10,
                 ),
                 Expanded(
-                  child: Primary_Card(
-                    selected_color: Color(0XFF1D1E33),
-                    inside_card: Icon_and_name(
-                      icon_select: FontAwesomeIcons.mars,
-                      icon_name: 'MALE',
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    child: Primary_Card(
+                      selected_color: selectedGender == Gender.male
+                          ? activatedCardColor
+                          : inactivatedCardColor,
+                      inside_card: Icon_and_name(
+                        icon_select: FontAwesomeIcons.mars,
+                        icon_name: 'MALE',
+                      ),
                     ),
                   ),
                 ),
                 Expanded(
-                  child: Primary_Card(
-                    selected_color: Color(0XFF1D1E33),
-                    inside_card: Icon_and_name(
-                      icon_select: FontAwesomeIcons.venus,
-                      icon_name: 'FEMALE',
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    child: Primary_Card(
+                      selected_color: selectedGender == Gender.female
+                          ? activatedCardColor
+                          : inactivatedCardColor,
+                      inside_card: Icon_and_name(
+                        icon_select: FontAwesomeIcons.venus,
+                        icon_name: 'FEMALE',
+                      ),
                     ),
                   ),
                 ),
@@ -56,7 +83,7 @@ class _InputPageState extends State<InputPage> {
                   width: 10,
                 ),
                 Expanded(
-                  child: Primary_Card(selected_color: Color(0XFF1D1E33)),
+                  child: Primary_Card(selected_color: activatedCardColor),
                 ),
                 SizedBox(
                   width: 10,
@@ -70,10 +97,10 @@ class _InputPageState extends State<InputPage> {
                   width: 10,
                 ),
                 Expanded(
-                  child: Primary_Card(selected_color: Color(0XFF1D1E33)),
+                  child: Primary_Card(selected_color: activatedCardColor),
                 ),
                 Expanded(
-                  child: Primary_Card(selected_color: Color(0XFF1D1E33)),
+                  child: Primary_Card(selected_color: activatedCardColor),
                 ),
                 SizedBox(
                   width: 10,
@@ -92,4 +119,3 @@ class _InputPageState extends State<InputPage> {
         ));
   }
 }
-
